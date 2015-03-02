7545cdde03569bf5ebeb2eb53c670e549ba8524b
jason.tao.wang <jason.tao.wang@347fdae5-b0b1-738e-1fe5-3e19eabe57bc>
2015-03-02 08:37:10 +0000

    added regfile.sv, modified CPU.sv

--- ALU.sv
+++ ALU.sv
@@ -1,19 +1,16 @@
 module ALU (input	[15:0]	A_in, B_in,
-			input	[2:0]	F,
+			input	[1:0]	F,
 			output	[15:0]	F_A_B);
 
-	always_comb
-		begin
-			unique case(F)
-				3'b000:		F_A_B = A_In + B_In;
-				3'b001: 	F_A_B = A_In - B_In;
-				3'b010: 	F_A_B = A_In & B_In;
-				3'b011: 	F_A_B = A_In | B_In;
-				3'b100: 	F_A_B = A_In ^ B_In;
-				3'b101: 	F_A_B = ~A_In;
-				3'b110: 	F_A_B = 1'b0;
-				default: 	F_A_B = A_In;
-			endcase
-		end
+		always_comb
+			begin
+				unique case(F)
+					2'b00:		F_A_B = A_In + B_In;
+					2'b01: 		F_A_B = A_In & B_In;
+					2'b10: 		F_A_B = ~A_In;
+					2'b11:	 	F_A_B = 1'b0;
+					default: 	F_A_B = A_In;
+				endcase
+			end
 
 endmodule
