3799abf9fc9fb82e5fae5c7728638b3859a5f385
jason.tao.wang <jason.tao.wang@347fdae5-b0b1-738e-1fe5-3e19eabe57bc>
2015-03-04 01:50:16 +0000

    added nzp_logic, still working on CPU and ISDU

---
+++ nzp_logic.sv
@@ -0,0 +1,30 @@
+module nzp_logic (input		[15:0]	Data,
+						input					LD_CC,
+						output				n, z, p);
+
+		always_ff @ (posedge LD_CC)
+		begin
+			if (LD_CC)
+			begin
+				if (Data == 16'h0000)
+				begin
+					n <= 1'b0;
+					z <= 1'b1;
+					p <= 1'b0;
+				end
+				else if (Data[15] == 1'b1)
+				begin
+					n <= 1'b1;
+					z <= 1'b0;
+					p <= 1'b0;
+				end
+				else if (Data[15] == 1'b0)
+				begin
+					n <= 1'b0;
+					z <= 1'b0;
+					p <= 1'b1;
+				end
+			end
+		end
+		
+endmodule
